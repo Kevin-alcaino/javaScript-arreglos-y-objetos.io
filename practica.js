f49e777c96e146d52b6fe8fe58e71d657f831bc1
Kevin-alcaino <wthblck1@gmail.com>
2026-08-01 14:07:34 -0400

    se enlazan los botones y los apartados de Propiedades en alquiler y venta

---
+++ practica.js
@@ -0,0 +1,5 @@
+const sucursales = ["Rebeca Mate 18", "Libertad 5", "Av manquehue Sur 31"]
+const codigosDeDescuento = ["ABC","FE1","8IA"]
+const clientesPremium = ["Andrea", "Fernando", "Mariel"]
+
+console.log (clientesPremium)
