0c5bdfa84b1dc0a6397339dc7f118bc7a2314e47
Kevin-alcaino <wthblck1@gmail.com>
2026-08-02 16:23:18 -0400

    se crean la constante respectiva para alquiler y ventes con su js individual

--- assets/javaScript/alquiler.js
+++ assets/javaScript/alquiler.js
@@ -1,43 +1,46 @@
 const propiedades_alquiler = [
 {
-nombre: '',
-src: '',
-descripcion: '',
-ubicacion: '',
-habitaciones: 0,
-costo: 0,
+nombre: 'Apartamento en el centro de la ciudad',
+src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+ubicacion: '123 Main Street, Anytown, CA 91234',
+habitaciones: 2,
+baños: 2,
+costo: 2.000,
 smoke: false,
 pets: true
 },
 {
-nombre: '',
-src: '',
-descripcion: '',
+nombre: 'Apartamento luminoso con vista al mar',
+src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
 ubicacion: '',
-habitaciones: 0,
-costo: 0,
-smoke: false,
+habitaciones: 3,
+baños: 3,
+costo: 2.500,
+smoke: true,
 pets: true
 },
 {
-nombre: '',
-src: '',
-descripcion: '',
-ubicacion: '',
-habitaciones: 0,
-costo: 0,
+nombre: 'Condominio moderno en zona residencial',
+src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+ubicacion: '123 Main Street, Anytown, CA 91234',
+habitaciones: 2,
+baños: 2,
+costo: 2.200,
 smoke: false,
-pets: true
+pets: false
 },
 {
-nombre: '',
-src: '',
-descripcion: '',
-ubicacion: '',
-habitaciones: 0,
-costo: 0,
-smoke: false,
+nombre: 'Condominio moderno en la nieve',
+src: 'https://lbcdn.airpaz.com/hotelimages/1368618/casa-refugio-farellones-9697a9643c31c1d39bcdc5947055f289.jpg',
+descripcion: 'Este condominio moderno está ubicado en una zona de alta montaña, en la cordillera de los Andes,Chile.',
+ubicacion: 'Camino de Los Condores 1180, Farellones 7690000',
+habitaciones: 2,
+baños: 2,
+costo: 1.800,
+smoke: true,
 pets: true
-}
+} 
 ]
-
