bb4e76cad2b7a54f3b7f3a72b7b8d4e3cfaba93a
Kevin-alcaino <wthblck1@gmail.com>
2026-08-02 15:26:06 -0400

    se crean 3 archivos js uno para alquiler,venta y otro el index

---
+++ assets/javaScript/alquiler.js
@@ -0,0 +1,43 @@
+const propiedades_alquiler = [
+{
+nombre: '',
+src: '',
+descripcion: '',
+ubicacion: '',
+habitaciones: 0,
+costo: 0,
+smoke: false,
+pets: true
+},
+{
+nombre: '',
+src: '',
+descripcion: '',
+ubicacion: '',
+habitaciones: 0,
+costo: 0,
+smoke: false,
+pets: true
+},
+{
+nombre: '',
+src: '',
+descripcion: '',
+ubicacion: '',
+habitaciones: 0,
+costo: 0,
+smoke: false,
+pets: true
+},
+{
+nombre: '',
+src: '',
+descripcion: '',
+ubicacion: '',
+habitaciones: 0,
+costo: 0,
+smoke: false,
+pets: true
+}
+]
+
--- practica.js
+++
@@ -1,5 +0,0 @@
-const sucursales = ["Rebeca Mate 18", "Libertad 5", "Av manquehue Sur 31"]
-const codigosDeDescuento = ["ABC","FE1","8IA"]
-const clientesPremium = ["Andrea", "Fernando", "Mariel"]
-
-console.log (clientesPremium)
