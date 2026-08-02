dd66d95dcb87b92c3bb650181a8f2d8a119c3118
Kevin-alcaino <wthblck1@gmail.com>
2026-08-02 18:40:42 -0400

    se crea el archivo js de ventas  y se enlaza al index.js

---
+++ assets/javaScript/index.js
@@ -0,0 +1,44 @@
+
+import { propiedades_venta } from "./venta.js";
+
+const contenedor = document.querySelector("#venta");
+
+function mostrarVentas() {
+    let html = '<div class="row">';
+
+    for (const propiedad of propiedades_venta) {
+        html += `
+        
+        
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <img src="${propiedad.src}" class="card-img-top" alt="${propiedad.nombre}">
+                <div class="card-body">
+                    <h5 class="card-title">${propiedad.nombre}</h5>
+                    <p class="card-text">${propiedad.descripcion}</p>
+                    <p>
+                        <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                    </p>
+                    <p>
+                        <i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |
+                        <i class="fas fa-bath"></i> ${propiedad.baños} Baños
+                    </p>
+                    <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
+                    
+                    <p class="${propiedad.smoke ? 'text-success' : 'text-danger'}">
+                        <i class="${propiedad.smoke ? 'fas fa-smoking' : 'fas fa-smoking-ban'}"></i> 
+                        ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'}
+                    </p>
+                    <p class="${propiedad.pets ? 'text-success' : 'text-danger'}">
+                        <i class="${propiedad.pets ? 'fas fa-paw' : 'fa-solid fa-ban'}"></i> 
+                        ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'}
+                    </p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    contenedor.innerHTML = html;
+}
+
+mostrarVentas();
